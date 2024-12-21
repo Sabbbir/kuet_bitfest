0c8a8ecdfc9041ce957ac31ed3e472187a7239b9
sabbbirr <sabbir.ahmed1925@gmail.com>
2024-12-21 21:55:12 +0600

    model trained

--- Challenge 1 Folder/total.py
+++ Challenge 1 Folder/total.py
@@ -1,49 +1,67 @@
-import datasets
-from transformers import MBartForConditionalGeneration, MBart50TokenizerFast, Trainer, TrainingArguments
-from sklearn.model_selection import train_test_split
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
+from datasets import load_dataset
 
-# Load the dataset
-dataset = datasets.load_dataset("SKNahin/bengali-transliteration-data")
+# Load Dataset
+dataset = load_dataset("SKNahin/bengali-transliteration-data")
 
-# Split the dataset into training and validation subsets
-train_data, val_data = train_test_split(dataset['train'], test_size=0.1)
+data = dataset['train'].train_test_split(test_size=0.1, seed=42)
+train_data = data['train']
+val_data = data['test']
 
-# Tokenize the data
-tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+# Preprocessing
+model_name = "google/mt5-small"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-def tokenize_function(examples):
-    inputs = [ex for ex in examples['banglish']]
-    targets = [ex for ex in examples['bangla']]
-    model_inputs = tokenizer(inputs, max_length=128, truncation=True)
+
+def preprocess_function(examples):
+    inputs = [ex for ex in examples["bn"]]
+    targets = [ex for ex in examples["rm"]]
+    model_inputs = tokenizer(inputs, max_length=128,
+                             truncation=True, padding=True)
     with tokenizer.as_target_tokenizer():
-        labels = tokenizer(targets, max_length=128, truncation=True)
+        labels = tokenizer(targets, max_length=128,
+                           truncation=True, padding=True)
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
-train_dataset = train_data.map(tokenize_function, batched=True)
-val_dataset = val_data.map(tokenize_function, batched=True)
 
-# Select a model
-model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+train_dataset = train_data.map(
+    preprocess_function, batched=True, remove_columns=train_data.column_names)
+val_dataset = val_data.map(
+    preprocess_function, batched=True, remove_columns=val_data.column_names)
 
-# Training arguments
-training_args = TrainingArguments(
+# Load Model
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+# Training Arguments
+training_args = Seq2SeqTrainingArguments(
     output_dir="./results",
     evaluation_strategy="epoch",
-    learning_rate=2e-5,
+    learning_rate=5e-5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
     num_train_epochs=3,
     weight_decay=0.01,
+    save_total_limit=3,
+    predict_with_generate=True,
+    logging_dir="./logs",
+    load_best_model_at_end=True,
+    fp16=torch.cuda.is_available(),
 )
 
 # Trainer
-trainer = Trainer(
+trainer = Seq2SeqTrainer(
     model=model,
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=val_dataset,
+    tokenizer=tokenizer,
 )
 
-# Train the model
+# Train
 trainer.train()
+
+# Save the model
+model.save_pretrained("./banglish_to_bangla_model")
+tokenizer.save_pretrained("./banglish_to_bangla_model")
